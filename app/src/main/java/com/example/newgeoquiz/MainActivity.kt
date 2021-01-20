173b09445d9e16e0cf54661a0ebeddc7f00a2a24
sergey-ozernyy <sergey.ozernyy@mail.ru>
2021-01-21 01:40:37 +0300

    Пройдена 4 глава

--- app/src/main/java/com/example/newgeoquiz/MainActivity.kt
+++ app/src/main/java/com/example/newgeoquiz/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.newgeoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+
+private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,20 +22,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
 
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true)
-    )
-
-    private var currentIndex = 0
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX) ?: 0
+        quizViewModel.currentIndex = currentIndex
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -43,7 +46,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener{ view:View ->
-            currentIndex = (currentIndex + 1)% questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
         }
 
@@ -52,11 +55,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateQuestion(){
-        val questionTextResId = questionBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
         }
     private fun checkAnswer(userAnswer: Boolean){
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
 
     val messageResId = if (userAnswer == correctAnswer) {
         R.string.correct_toast
@@ -65,5 +68,20 @@ class MainActivity : AppCompatActivity() {
         R.string.incorrect_toast
     }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle){
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG, "onSaveInstanceState")
+        savedInstanceState.putInt(KEY_INDEX, quizViewModel.currentIndex)
+    }
+
+    override fun onStop() {
+        super.onStop()
     }
 }
